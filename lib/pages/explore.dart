eefa228077ae3f88016b32e9c49a09f0f2490262
goodboop86 <goodboop86@gmail.com>
2025-03-26 21:32:52 +0900

    [doing]

--- lib/pages/explore.dart
+++ lib/pages/explore.dart
@@ -28,84 +28,12 @@ class ExplorePage extends Component
     Sizes().setScreenSize(game.size);
 
     await _addCharacters();
-    _addButtons();
     List<List<Event>> stageList = generateNestedListWithFixedLength(11, 1, 3);
     _addMap(stageList, currentStage);
     _addMapCards(stageList, currentStage);
   }
 
-  void _enemyTurn() {
-    final card = ActionCard(
-      name: 'EnemyCard',
-      effect: PlayerDamageEffect(),
-    );
 
-    final timerComponent = TimerComponent(
-      period: 1,
-      onTick: () async {
-        card.effect.call(ref);
-
-        await Future.delayed(const Duration(seconds: 1));
-
-        _playerTurn();
-      },
-      removeOnFinish: true,
-    );
-
-    // RectangleComponentをTimerComponentの子として追加
-    timerComponent.add(RectangleComponent(
-      size: Vector2(200, 200),
-      position: Sizes().screenSize / 2,
-      anchor: Anchor.center,
-      paint: Paint()..color = Colors.black87,
-    ));
-
-    // TextComponentをTimerComponentの子として追加
-    timerComponent.add(TextComponent(
-      text: 'Enemy Turn',
-      position: Sizes().screenSize / 2,
-      anchor: Anchor.center,
-      textRenderer: TextPaint(style: const TextStyle(color: Colors.white)),
-    ));
-
-    // TimerComponentをゲームに追加
-    add(timerComponent);
-
-    // タイマーを開始
-    timerComponent.timer.start();
-  }
-
-  void _playerTurn() {
-    final timerComponent = TimerComponent(
-      period: 1,
-      onTick: () {
-        debugPrint("PlayerTurn start!");
-      },
-      removeOnFinish: true,
-    );
-
-    // RectangleComponentをTimerComponentの子として追加
-    timerComponent.add(RectangleComponent(
-      size: Vector2(200, 200),
-      position: Sizes().screenSize / 2,
-      anchor: Anchor.center,
-      paint: Paint()..color = Colors.black87,
-    ));
-
-    // TextComponentをTimerComponentの子として追加
-    timerComponent.add(TextComponent(
-      text: 'Player Turn',
-      position: Sizes().screenSize / 2,
-      anchor: Anchor.center,
-      textRenderer: TextPaint(style: const TextStyle(color: Colors.white)),
-    ));
-
-    // TimerComponentをゲームに追加
-    add(timerComponent);
-
-    // タイマーを開始
-    timerComponent.timer.start();
-  }
 
   Future<void> _addCharacters() async {
     final characterArea = CharacterAreaComponent(
@@ -138,56 +66,6 @@ class ExplorePage extends Component
     player.add(PlayerHpBar());
   }
 
-  void _addButtons() {
-    // カードエリアを作成
-    final buttonArea = ButtonAreaComponent(
-      position: Sizes().buttonAreaPosition,
-      size: Sizes().buttonAreaSize, // カードエリアのサイズ
-    );
-    add(buttonArea);
-
-    List buttonOnPressedFunctions = [
-      () {
-        game.router.pushNamed('home');
-      },
-      () {
-        debugPrint('Button 2 pressed');
-      },
-      () {
-        _enemyTurn();
-      }
-    ];
-
-    final buttonAreaCenterX = Sizes().buttonAreaWidth / 2;
-    final buttonAreaCenterY = Sizes().buttonAreaHeight / 2;
-    buttonOnPressedFunctions.asMap().forEach((index, function) {
-      final button = ButtonComponent(
-        button: RectangleComponent(
-            size: Sizes().buttonSize,
-            paint: Paint()..color = Colors.brown,
-            priority: 0),
-        onPressed: function,
-        children: [
-          TextComponent(
-            priority: 1,
-            text: '$index',
-            position: Sizes().buttonSize / 2,
-            anchor: Anchor.center,
-            textRenderer:
-                TextPaint(style: const TextStyle(color: Colors.white)),
-          ),
-        ],
-      )
-        ..position = Vector2(
-          buttonAreaCenterX +
-              index * (Sizes().buttonWidth + Sizes().margin) -
-              (Sizes().buttonWidth + Sizes().margin), // X 座標を調整
-          buttonAreaCenterY, // Y 座標を調整
-        )
-        ..anchor = Anchor.center;
-      buttonArea.add(button);
-    });
-  }
 
   void _addMap(List<List<Event>> stageList, int currentStage) {
     final mapArea = MapAreaComponent(
@@ -258,9 +136,37 @@ class ExplorePage extends Component
 
       final row = index ~/ 3;
       final col = index % 3;
-      final cardComponent = MapCardComponent(name: event.name)
-        ..size = Sizes().mapCardSize
-        ..anchor = Anchor.center
+      // final cardComponent = MapCardComponent(name: event.name)
+      //   ..size = Sizes().mapCardSize
+      //   ..anchor = Anchor.center
+      //   ..position = Vector2(
+      //     cardAreaCenterX +
+      //         col * (Sizes().mapCardWidth + Sizes().mapCardMargin) -
+      //         (Sizes().mapCardWidth + Sizes().mapCardMargin), // X 座標を調整
+      //     cardAreaCenterY +
+      //         row * (Sizes().mapCardHeight + Sizes().mapCardMargin) -
+      //         (Sizes().mapCardHeight + Sizes().mapCardMargin) / 2, // Y 座標を調整
+      //   ); // カードエリアの中心を基準に位置を計算
+
+      final button = ButtonComponent(
+        button: RectangleComponent(
+            size: Sizes().mapCardSize,
+            paint: Paint()..color = Colors.green,
+            priority: 0),
+        onPressed:() {
+          game.router.pushNamed('battle');
+        },
+        children: [
+          TextComponent(
+            priority: 1,
+            text: event.name,
+            anchor: Anchor.center,
+            position: Sizes().mapCardSize/2,
+            textRenderer:
+            TextPaint(style: const TextStyle(color: Colors.white)),
+          ),
+        ],
+      )..anchor = Anchor.center
         ..position = Vector2(
           cardAreaCenterX +
               col * (Sizes().mapCardWidth + Sizes().mapCardMargin) -
@@ -268,24 +174,10 @@ class ExplorePage extends Component
           cardAreaCenterY +
               row * (Sizes().mapCardHeight + Sizes().mapCardMargin) -
               (Sizes().mapCardHeight + Sizes().mapCardMargin) / 2, // Y 座標を調整
-        ); // カードエリアの中心を基準に位置を計算
-      mapCardArea.add(cardComponent);
+        );
+      mapCardArea.add(button);
 
     });
-  }
-
-  void rearrangeCards() {
-    final totalCardWidth = _cards.length * Sizes().cardWidth;
-    final totalMarginWidth = (_cards.length - 1) * Sizes().cardMargin;
-    final totalWidth = totalCardWidth + totalMarginWidth;
-
-    final startX = (Sizes().screenWidth - totalWidth) / 2;
-
-    for (int i = 0; i < _cards.length; i++) {
-      _cards[i].position = Vector2(
-          startX + i * (Sizes().cardWidth + Sizes().cardMargin),
-          Sizes().cardHeight);
-    }
   }
 
   void setCallback(Function fn) => stateCallbackHandler = fn;
