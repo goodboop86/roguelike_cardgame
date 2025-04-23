c554478d37df216e48e6ab69e9eacf98c729911b
goodboop86 <goodboop86@gmail.com>
2025-04-23 21:36:16 +0900

    [doing]

--- lib/components/text_component.dart
+++ lib/components/text_component.dart
@@ -20,3 +20,23 @@ class DialogText extends TextBoxComponent {
     ));
   }
 }
+
+
+class SupportText extends TextBoxComponent {
+  SupportText() {
+    size = Sizes.supportDialogSize;
+    align = Anchor.center;
+    boxConfig = const TextBoxConfig(
+        timePerChar: 0.03,
+        maxWidth: 300.0,
+        growingBox: true,
+        margins: EdgeInsets.fromLTRB(10, 10, 10, 10));
+
+    textRenderer = TextPaint(
+        style: const TextStyle(
+          fontSize: 14,
+          color: Colors.black,
+          fontFamily: 'monospace', // 等幅フォントが見やすい
+        ));
+  }
+}
--- lib/mixin/has_person_area.dart
+++ lib/mixin/has_person_area.dart
@@ -1,4 +1,5 @@
 import 'package:flame/components.dart';
+import 'package:flame/effects.dart';
 import 'package:flame/game.dart';
 import 'package:flame_riverpod/flame_riverpod.dart';
 import 'package:flutter/material.dart';
@@ -30,21 +31,47 @@ mixin HasPersonArea on Component, HasGameRef<MainGame>, RiverpodComponentMixin {
       ..onComplete = () async {
         String value = await npcDialog();
 
-        add(TextBoxComponent(
-            align: Anchor.center,
-            text: value,
+        RectangleComponent valueText = RectangleComponent(
+            position: Sizes.supportDialogPosition,
             size: Sizes.supportDialogSize,
-            anchor: Anchor.topLeft,
-            position: Sizes.supportDialogPosition));
+            paint: Paint()..color = Colors.red.withValues(alpha: 0.1),
+            children: [
+          TextBoxComponent(
+              align: Anchor.center,
+              text: value,
+              size: Sizes.supportDialogSize,
+              anchor: Anchor.topLeft,
+              // position: Sizes.supportDialogPosition
+          )
+        ]);
+
+        valueText.add(MoveEffect.by(
+          Vector2(0, -5), // 上方向に50ピクセル移動
+          EffectController(
+            duration: 1.0, // 2秒かけて移動
+            curve: Curves.easeInOut, // イーズイン・アウトのアニメーションカーブ
+          ),
+        ));
+
+        valueText.add(OpacityEffect.to(
+          1.0, // 最終的な透明度（完全に不透明）
+          EffectController(
+            duration: 1.0, // 2秒かけて透明度を変化
+            curve: Curves.easeInOut,
+          ),
+        ));
+
+        add(valueText);
 
         dialog.remove(textComponent1);
 
         textComponent1 = DialogText()
           ..text = 'またおいで..............'
-          ..onComplete = () async {log.info("end text.");};
+          ..onComplete = () async {
+            log.info("end text.");
+          };
 
         dialog.add(textComponent1);
-
       };
 
     dialog.add(textComponent1);
